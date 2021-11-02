2f8764c323e204a09918902115ca611e6896e82f
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2021-11-02 13:17:03 -0300

    Finish application (GET last 3 messages route, GET user profile)

--- node_heat/src/routes.ts
+++ node_heat/src/routes.ts
@@ -1,16 +1,22 @@
-import { Router } from "express";
-import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-import { CreateMessageController } from "./controllers/CreateMessageController";
-import { ensureAuthenticated } from "./controllers/middleware/ensureAuthenticated";
+import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
+import { ensureAuthenticated } from './controllers/middleware/ensureAuthenticated';
+import { ProfileUserController } from './controllers/ProfileUserController';
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle);
+router.post('/authenticate', new AuthenticateUserController().handle);
 
 router.post(
-  "/messages",
+  '/messages',
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+
+router.get('/messages/last3', new GetLast3MessagesController().handle);
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
