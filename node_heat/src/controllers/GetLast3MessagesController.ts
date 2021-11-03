b6cfb439691d942239a4cf70a763b9de24b9b10e
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2021-11-02 21:07:55 -0300

    Start react class 13min30seg

--- node_heat/src/controllers/GetLast3MessagesController.ts
+++ node_heat/src/controllers/GetLast3MessagesController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import { GetLast3MessagesService } from '../services/GetLast3MessagesService';
 
 class GetLast3MessagesController {
