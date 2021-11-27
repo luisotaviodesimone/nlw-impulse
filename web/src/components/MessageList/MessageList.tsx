dcf9bd2e615a3957cf76594a845c3fc429cf6143
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2021-11-27 12:33:05 -0300

    finish stage 2

--- web/src/components/MessageList/MessageList.tsx
+++ web/src/components/MessageList/MessageList.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 import logoImg from '../../assets/logo.svg';
 import { api } from '../../services/api';
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 type Message = {
   id: string;
@@ -12,8 +13,28 @@ type Message = {
   };
 };
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:3005');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift()
+      }
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then((response: any) => {
@@ -29,16 +50,11 @@ export function MessageList() {
         {messages.map((message) => {
           return (
             <li key={message.id} className={styles.message}>
-              <p className={styles.messageContent}>
-                {message.text}
-              </p>
+              <p className={styles.messageContent}>{message.text}</p>
 
               <div className={styles.messageUser}>
                 <div className={styles.userImage}>
-                  <img
-                    src={message.user.avatar_url}
-                    alt={message.user.name}
-                  />
+                  <img src={message.user.avatar_url} alt={message.user.name} />
                 </div>
                 <span>{message.user.name}</span>
               </div>
